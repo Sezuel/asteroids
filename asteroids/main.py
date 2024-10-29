c7f4b63d0080413af26325bd1d142db12e0ad3c9
Sezuel <jdragon660@yahoo.com>
2024-10-29 11:49:47 -0700

    Moveable player

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -10,12 +11,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = float(2)
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
+        for obj in updateable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)/1000)
 
 if __name__ == "__main__":
     main()
